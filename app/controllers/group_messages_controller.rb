6fe12a810fa5338af9a3511f01a8f2d4d5763bd6
Gaston de la Fare <gastondelafare@gmail.com>
2024-12-13 17:26:53 -0300

    chat almost done

--- app/controllers/group_messages_controller.rb
+++ app/controllers/group_messages_controller.rb
@@ -7,17 +7,18 @@ class GroupMessagesController < ApplicationController
 
   def create_message
     @group = Group.find(params[:id])
+    @messages = GroupMessage.where(group: @group)
     @message = GroupMessage.new(message_params)
     @message.user = current_user
     @message.group = @group
     if @message.save
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.append(:messages, partial: "group_messages/message",
-                                                              locals: { message: @message })
-        end
-        format.html { redirect_to messages_path }
-      end
+      # respond_to do |format|
+      # format.turbo_stream do
+      #   render turbo_stream: turbo_stream.update(:messages, partial: "group_messages/message",
+      #                                                       locals: { message: @message, user: current_user })
+      # end
+      redirect_to messages_path
+      # end
     else
       render :index, status: :unprocessable_entity
     end
