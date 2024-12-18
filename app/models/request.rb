e3372ceedfc7f8c48e9c8d3f65a30cb9ba8b4eca
Gaston de la Fare <gastondelafare@gmail.com>
2024-12-18 18:17:51 -0300

    notifications for new invites to groups

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,16 @@ class Request < ApplicationRecord
   belongs_to :user
   belongs_to :event
   belongs_to :group
+  after_create_commit :broadcast_notification
 
   validates :user, uniqueness: { scope: %i[group status] }
+
+  def broadcast_notification
+    return unless status == "pending_join"
+
+    broadcast_append_to "profile_#{user.id}_notifications",
+                        partial: "shared/profile_notification_dot",
+                        target: "profile-dropdown",
+                        locals: { user: user }
+  end
 end
